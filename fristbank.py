20e8fe91692d078e7a5bb18edef28f7f2794f993
ingridalvesz <ingrid.roberta.alves.s@outlook.com>
2023-10-16 17:07:01 -0300

    #students_add

--- fristbank.py
+++ fristbank.py
@@ -5,8 +5,8 @@ banco = sqlite3.connect('frist_bank.db')
 cursor = banco.cursor()
 
 
-cursor.execute("CREATE TABLE alunos (nome text, idade integer, email text)")
+#cursor.execute("CREATE TABLE alunos (nome text, idade integer, email text)")
 
-cursor.execute("INSERT INTO alunos VALUES('bela', 25, 'ingridbenz32@gmail.com')")
+cursor.execute("INSERT INTO alunos VALUES('Thaise', 21, 'thaiseed@gmail.com')")
 
 banco.commit()
